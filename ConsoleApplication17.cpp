8ac02cc4a6bd4aea116e41156ce029c8c7b00416
maks228998 <78788414+maks228998@users.noreply.github.com>
2021-03-19 19:20:14 +0600

    Add files via upload

---
+++ ConsoleApplication17.cpp
@@ -0,0 +1,34 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<cstdlib>
+#include "my_lib.h"
+
+int main() {
+	printf("Enter the bit sequences\n");
+	int number = get_int_number_than_greater_one();
+	
+	int multiplier = 256;
+	int result = 0;
+
+	_asm {
+		mov ecx, 8
+		mov ebx, 2
+
+			point_one:
+
+				mov eax, number
+				mov edx, 0
+				div ebx;
+				imul edx, multiplier
+				add result, edx
+				mov number, eax
+				mov eax, multiplier
+				mov edx, 0
+				div ebx;
+				mov multiplier, eax
+				mov eax, number
+
+			loop point_one 
+	}
+	
+	printf("Result: %d", result);
+}
